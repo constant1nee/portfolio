0199c4e2e0d0b1ba141ed223f32e7fb0021f4c23
constant1nee <149838917+constant1nee@users.noreply.github.com>
2023-11-04 00:48:33 +0300

    Delete Financial Assistant/It Wants To EAT directory

--- Financial Assistant/It Wants To EAT/data/sandbox.py
+++
@@ -1,8 +0,0 @@
-import pygame
-
-pygame.init()
-pygame.display.set_caption('It Wants To EAT')
-size = width, height = 600, 900
-screen = pygame.display.set_mode(size)
-screen.fill((20, 20, 20))
-running = True
