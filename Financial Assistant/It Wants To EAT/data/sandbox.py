0198e7a5663d6d6322ebd319b2509543e0ca4929
constant1nee <149838917+constant1nee@users.noreply.github.com>
2023-11-04 00:45:19 +0300

    Add files via upload

---
+++ Financial Assistant/It Wants To EAT/data/sandbox.py
@@ -0,0 +1,8 @@
+import pygame
+
+pygame.init()
+pygame.display.set_caption('It Wants To EAT')
+size = width, height = 600, 900
+screen = pygame.display.set_mode(size)
+screen.fill((20, 20, 20))
+running = True
